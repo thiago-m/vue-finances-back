a1b9eb93e2e0fdebc8198108e9a91fa30eb3f48c
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-15 07:45:00 -0300

    organizing the files

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ const { GraphQLServer } = require('graphql-yoga')
 const Binding = require('prisma-binding')
 const { prisma } = require('./generated/prisma-client')
 
-const binding = new Binding.Prisma({
-  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
-  endpoint: process.env.PRISMA_ENDPOINT
-})
-
-const resolvers = {
-  Query: {
-    user(parent, args, context, info) {
-      return binding.query.user({where: {id: args.id}}, info)
-    }
-  }
-}
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
-  resolvers
+  resolvers,
+  context: {
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: process.env.PRISMA_ENDPOINT
+    }),
+    prisma
+  }
 })
 
 server.start().then(() => console.log('Server On in "http://localhost:4000/"'))
