0a51ae985a2f20042d74155cc999f7d16eb13dea
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-16 07:17:42 -0300

    added Mutation login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,18 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+async function login (_, {email, password}, ctx, info) {
+  const user = await ctx.db.query.user({ where: {email} })
+  if(!user) throw new Error('invalid credentials!')
+
+  const valid = await bcrypt.compare(password, user.password)
+  if(!valid) throw new Error('invalid credentials!')
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '2h' })
+
+  return { token, user }
+}
+
 async function signup (_, args, ctx, info) {
   const password = await bcrypt.hash(args.password, 10)
   const user = await ctx.db.mutation.createUser({data: { ...args, password }})
@@ -12,4 +24,4 @@ async function signup (_, args, ctx, info) {
   return { token, user }
 }
 
-module.exports = { signup }
+module.exports = { login, signup }
