cafec854a87249dde79b9b959ef19d17ce22abc1
Thiago Marciel <thiagomarciel.dev@gmail.com>
2022-03-16 07:50:37 -0300

    added Mutation createAccount

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,20 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { getUserId } = require('./../utils')
 
 const JWT_SECRET = process.env.JWT_SECRET
+
+async function createAccount(_, {description}, ctx, info) {
+  const userId = getUserId(ctx)
+  return ctx.db.mutation.createAccount({
+    data: {
+      description,
+      user: {
+        connect: { id: userId }
+      }
+    }
+  }, info)
+}
 
 async function login (_, {email, password}, ctx, info) {
   const user = await ctx.db.query.user({ where: {email} })
@@ -24,4 +37,4 @@ async function signup (_, args, ctx, info) {
   return { token, user }
 }
 
-module.exports = { login, signup }
+module.exports = { createAccount, login, signup }
